cac86ff241410d6090ff5d75ab2b3a160f17c0e5
“Chuka <chuka.oraekwuotu@gmail.com>
2022-06-30 16:35:56 -0700

    betty doc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,6 @@
  */
 int main(void)
 {	 
-	
-	
-	int i;
-	long int; 
-	long long int ll;
-	float d;
-	char c;
-
-
-
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
