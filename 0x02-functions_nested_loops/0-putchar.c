c9bb0e41f50ed1a3881e1414f88b70d0bec8a63e
“Chuka <chuka.oraekwuotu@gmail.com>
2022-07-06 16:34:07 -0700

    correction 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - prints "anything"
+ * main - prints "putchar"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('a');
-	_putchar('n');
-	_putchar('y');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
+	_putchar('c');
 	_putchar('h');
-	_putchar('i');
-	_putchar('n');
-	_putchar('g');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
