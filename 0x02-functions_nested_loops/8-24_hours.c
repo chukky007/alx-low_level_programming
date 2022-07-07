7c5f9f7574397edca943bbcecbe3d70e5d3372bb
“Chuka <chuka.oraekwuotu@gmail.com>
2022-07-07 00:33:20 -0700

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; j < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
 			{
-				_putchar('0');
+				_putchar('0);
 				_putchar(i + '0');
 			}
 			else if (i >= 10)
@@ -23,13 +23,13 @@ void jack_bauer(void)
 			}
 			if (j < 10)
 			{
-				_putchar(':');
+				_putchar(' : ');
 				_putchar('0');
 				_putchar(j + '0');
 			}
 			else if (j >= 10)
 			{
-				_putchar(':');
+				_putchar(' : ');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
