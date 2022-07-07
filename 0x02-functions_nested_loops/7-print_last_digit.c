4e3a25acd0a02f9d026a8d719ec31335b9a8bc02
“Chuka <chuka.oraekwuotu@gmail.com>
2022-07-07 00:00:31 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,22 +2,21 @@
 
 /**
  * print_last_digits - prints the last digit of a number
- * @n: the int to extract the last digit from
+ * @n: the int to extract the last digit result
  * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
 	int a;
+	
 
-	if (n < 0)
-		n = -n;
-
-	a = n % 10;
+	a = (n % 10);
 
 	if (a < 0)
-		a = -a;
+	{
+		a = (-1 * a);
+	}
 
 	_putchar(a + '0');
-
 	return (a);
 }
