431331bb209a74fe98d704fb2ff84948777ab336
“Chuka <chuka.oraekwuotu@gmail.com>
2022-07-12 16:55:32 -0700

    correction2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -37,6 +37,14 @@ int main(void)
 				break;
 			}
 		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
 	}
 
 	printf("%s", password);
