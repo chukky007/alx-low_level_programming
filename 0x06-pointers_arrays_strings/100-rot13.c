9fe43894aa0dcb4395169bf0b5d37d2baa04eaca
“Chuka <chuka.oraekwuotu@gmail.com>
2022-07-14 17:30:06 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string using rot13
+ * @s: the striing to encode
+ *
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	char *c;
+	char shift;
+
+	for (c = s; *c; ++c)
+	{
+		shift 'A' + (*c & 32);
+		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+			*c = (*c - shift + 13) % 26 + shift;
+	}
+	return (s);
+}
